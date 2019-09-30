f61553f15c470c012b080017ace8ce03c9f2c6f5
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-30 15:14:52 -0300

    Criando Projeto e suas classes

---
+++ Encodable/Model/Viagem.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+
+class Viagem {
+    
+    //ATRIBUTOS
+    
+    let id: Int
+    let titulo: String
+    let quantidadeDias: Int
+    let preco: String
+    let localizacao: String
+    
+    //INIT
+    
+    init(_ id: Int, _ titulo: String, _ quantidadeDias: Int,_ preco: String, _ localizacao: String) {
+        
+        self.id = id
+        self.titulo = titulo
+        self.quantidadeDias = quantidadeDias
+        self.preco = preco
+        self.localizacao = localizacao
+    }
+}
+
